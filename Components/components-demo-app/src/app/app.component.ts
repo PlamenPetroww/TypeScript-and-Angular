fe4668277c11a8a29a38ccda3d496589479c3678
plamenpetroww <plamenpetroww@gmail.com>
2023-06-29 10:27:42 +0200

    const stream$

--- Components/components-demo-app/src/app/app.component.ts
+++ Components/components-demo-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 import { User } from './types/User';
 import { UserService } from './user.service';
 
@@ -20,7 +21,33 @@ export class AppComponent {
       this.userService.addUser(inputName, inputAge);
     }
 
-  
 }
+
+/* const o = new Observable((observer) => {
+  // observer.next(1000);
+  // observer.next(2000);
+  // observer.next(3000);
+})
+
+o.subscribe((data) => {
+  console.log('data from observer', data);
+}) */
+
+function interval(intervalValue: number) {
+  return new Observable((observer) => {
+    let counter = 0;
+    const timer = setInterval(() => {
+      observer.next(counter++);
+    }, intervalValue);
+
+    return () => {
+      clearInterval(timer);
+    };
+  });
+}
+
+const stream$ = interval(3000).subscribe((data) => {
+  console.log('data from observer', data)
+})
 
 
