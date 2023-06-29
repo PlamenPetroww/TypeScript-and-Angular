57066b0622e96e3cf784ec9c549233ca2613a38d
plamenpetroww <plamenpetroww@gmail.com>
2023-06-29 08:58:07 +0200

    make new order for solid princip

--- Components/components-demo-app/src/app/app.component.ts
+++ Components/components-demo-app/src/app/app.component.ts
@@ -42,34 +42,4 @@ export class AppComponent {
   }
 }
 
-class Wallet {
-  balance = 0;
-  name = '';
 
-  constructor(name: string, balance: number) {
-    this.balance = balance;
-    this.name = name;
-  }
-}
-
-class Car {
-  model = '';
-  
-  constructor(model: string) {
-    this.model = model;
-  }
-}
-
-class Person {
-  wallet: Wallet;
-  car: Car;
-
-  constructor(car: Car, wallet: Wallet, name: string) {
-    this.car = car;
-    this.wallet = wallet;
-  }
-}
-
-const lisasCar = new Car('BMW');
-const linasWallet = new Wallet('Lina', 500);
-const personLina = new Person(lisasCar, linasWallet, 'Lisa')
