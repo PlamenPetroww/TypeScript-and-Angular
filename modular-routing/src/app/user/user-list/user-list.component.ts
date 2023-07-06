8bd032e0b05ed428e0dbe435861d2741f60d289f
plamenpetroww <plamenpetroww@gmail.com>
2023-07-06 11:53:18 +0200

    set setTimeOut

--- modular-routing/src/app/user/user-list/user-list.component.ts
+++ modular-routing/src/app/user/user-list/user-list.component.ts
@@ -1,19 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
+import { User } from 'src/app/types/User';
+import { GlobalLoaderService } from 'src/app/core/global-loader/global-loader.service';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styleUrls: ['./user-list.component.css'],
 })
 export class UserListComponent implements OnInit {
+  userList: User[] = [];
 
-  constructor(private userService: UserService) {
-
-  }
+  constructor(
+    private userService: UserService,
+    private globalLoaderService: GlobalLoaderService
+  ) {}
   ngOnInit(): void {
-    this.userService.fetchUsers().subscribe(users => {
-      console.log(users)
-    })
+    this.globalLoaderService.showLoader();
+    //setTimeout(() => {
+      this.userService.fetchUsers().subscribe({
+          next: (users) => {
+          this.userList = users;
+          this.globalLoaderService.hideLoader();
+        },
+        error: (error) => {
+          console.log(`Error: ${error}`);
+          this.globalLoaderService.hideLoader();
+        },
+      });
+    //}, 1500);
   }
 }
