f8c36aa903b23edbbaa2ac3813bcf00be2bbc2e7
plamenpetroww <plamenpetroww@gmail.com>
2023-07-13 09:46:39 +0200

    set MouseEvent

--- directives-and-forms/src/app/highlight-on-move.directive.ts
+++ directives-and-forms/src/app/highlight-on-move.directive.ts
@@ -1,22 +1,39 @@
 import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
-  selector: '[appHighlightOnMove]'
+  selector: '[appHighlightOnMove]',
 })
 export class HighlightOnMoveDirective implements OnInit {
-
-  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
-
+  constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit(): void {
     console.log(this.elRef);
     // this.elRef.nativeElement.style.backgroundColor = 'yellow'
-  
-    this.renderer.setStyle(
+
+    // this.renderer.setStyle(
+    //   this.elRef.nativeElement,
+    //   'background-color',
+    //   'red'
+    // );
+
+    this.renderer.listen(
       this.elRef.nativeElement,
-      'background-color',
-      'red'
+      'mouseenter',
+      this.mouseEnterHandler
     );
 
+    this.renderer.listen(
+      this.elRef.nativeElement,
+      'mouseleave',
+      this.mouseLeaveHandler
+    );
   }
+    mouseEnterHandler(e: MouseEvent): void{
+      console.log('enter', e)
+    };
+
+    mouseLeaveHandler(e: MouseEvent): void {
+      console.log('leave', e)
+    }
+
 }
