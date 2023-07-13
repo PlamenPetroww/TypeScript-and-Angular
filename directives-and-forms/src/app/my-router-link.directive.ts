6875fee9ed6f911a7c164596e1704c6b3adfb8e0
plamenpetroww <plamenpetroww@gmail.com>
2023-07-13 10:53:50 +0200

    use appMyRouterLink to navigate to the login page or register page

--- directives-and-forms/src/app/my-router-link.directive.ts
+++ directives-and-forms/src/app/my-router-link.directive.ts
@@ -1,10 +1,28 @@
-import { Directive } from '@angular/core';
+import { Directive, OnInit, ElementRef, Renderer2 } from '@angular/core';
+import { Router } from '@angular/router';
+import { Input } from '@angular/core';
 
 @Directive({
-  selector: '[appMyRouterLink]'
+  selector: '[appMyRouterLink]',
 })
-export class MyRouterLinkDirective {
+export class MyRouterLinkDirective implements OnInit {
+  @Input() appMyRouterLink: string = '';
 
-  constructor() { }
+  constructor(
+    private elRef: ElementRef,
+    private renderer: Renderer2,
+    private router: Router
+  ) {}
 
+  ngOnInit(): void {
+    this.renderer.listen(
+      this.elRef.nativeElement,
+      'click',
+      this.clickHandler.bind(this)
+    );
+  }
+
+  clickHandler(e: MouseEvent) {
+    this.router.navigate([this.appMyRouterLink])
+  }
 }
