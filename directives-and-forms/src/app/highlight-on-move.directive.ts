11ccea4c1ba672554697f1d03ef03533a545c9bb
plamenpetroww <plamenpetroww@gmail.com>
2023-07-13 10:24:29 +0200

    add @HostListener

--- directives-and-forms/src/app/highlight-on-move.directive.ts
+++ directives-and-forms/src/app/highlight-on-move.directive.ts
@@ -1,13 +1,36 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHighlightOnMove]',
 })
-export class HighlightOnMoveDirective implements OnInit {
+export class HighlightOnMoveDirective implements OnInit, OnDestroy {
+
+  @HostListener('mouseover', ['$event']) mouseOverHandler(e: MouseEvent) {
+    console.log(e)
+  }
+
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
+
+  unsubscribeFromEvent: (() => void) [] = [];
 
   ngOnInit(): void {
     console.log(this.elRef);
+
+    this.unsubscribeFromEvent.push(
+      this.renderer.listen(
+        this.elRef.nativeElement,
+        'mouseenter',
+        this.mouseEnterHandler.bind(this)
+      )
+    );
+
+    this.unsubscribeFromEvent.push(
+      this.renderer.listen(
+        this.elRef.nativeElement,
+        'mouseenter',
+        this.mouseLeaveHandler.bind(this)
+      )
+    );
     // this.elRef.nativeElement.style.backgroundColor = 'yellow'
 
     // this.renderer.setStyle(
@@ -18,33 +41,30 @@ export class HighlightOnMoveDirective implements OnInit {
 
     this.renderer.listen(
       this.elRef.nativeElement,
-      'mouseenter',
-      this.mouseEnterHandler.bind(this)
-    );
-
-    this.renderer.listen(
-      this.elRef.nativeElement,
       'mouseleave',
       this.mouseLeaveHandler.bind(this)
     );
   }
 
-
   mouseEnterHandler(e: MouseEvent): void {
-    console.log('enter', e);
-    this.renderer.setStyle(
-      this.elRef.nativeElement,
-      'background-color',
-      'yellow'
-    );
+    // this.renderer.setStyle(
+    //   this.elRef.nativeElement,
+    //   'background-color',
+    //   'yellow'
+    // );
+    this.renderer.addClass(this.elRef.nativeElement, 'hightlight');
   }
 
   mouseLeaveHandler(e: MouseEvent): void {
-    console.log('leave', e);
-    this.renderer.setStyle(
-      this.elRef.nativeElement,
-      'background-color',
-      'blue'
-    );
+    // this.renderer.setStyle(
+    //   this.elRef.nativeElement,
+    //   'background-color',
+    //   'blue'
+    // );
+    this.renderer.removeClass(this.elRef.nativeElement, 'hightlight');
+  }
+
+  ngOnDestroy(): void {
+    
   }
 }
