1356c214204f29e3a1e03e0654eff6349e067e43
plamenpetroww <plamenpetroww@gmail.com>
2023-07-14 08:30:40 +0200

    add form.setValue and form.invalid

--- directives-and-forms/src/app/login/login.component.ts
+++ directives-and-forms/src/app/login/login.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-
+  submitHandler(form: NgForm): void {
+    if(form.invalid) { // If the form is not valid
+      return;
+    }
+    // Set only to string, when Authentication is correct
+    form.setValue({
+      email: '',
+      password: '',
+    })
+    const value: {email: string, password: string} = form.value;
+    // value to Authentication
+    console.log(value.email)
+    console.log(value.password)
+  }
 }
