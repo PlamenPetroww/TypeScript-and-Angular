1b2d0851815c3abb93ca6f7b03e7fbb93c8f5f16
plamenpetroww <plamenpetroww@gmail.com>
2023-07-17 17:54:32 +0200

    remove action and method from the register page

--- Components-Workshop/components-workshop/src/app/user/register/register.component.ts
+++ Components-Workshop/components-workshop/src/app/user/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder, NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+
+  form = this.fb.group({
+    email: ['emailTest'],
+    username: ['usernameTest'],
+    tel: ['telTest'],
+    passGroup: this.fb.group({
+      password: ['pass'],
+      rePAssword: ['pass']
+    })
+
+  })
+
+  constructor(private fb: FormBuilder) {}
+
+  register(): void {
+    console.log(this.form.value)
+  }
+
 
 }
