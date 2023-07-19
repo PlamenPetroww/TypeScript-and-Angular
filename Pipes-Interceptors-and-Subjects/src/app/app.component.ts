7f6d9c5f7c79baf1dce641b766473f495d9c21b2
plamenpetroww <plamenpetroww@gmail.com>
2023-07-19 10:57:42 +0200

    added reduce function

--- Pipes-Interceptors-and-Subjects/src/app/app.component.ts
+++ Pipes-Interceptors-and-Subjects/src/app/app.component.ts
@@ -3,13 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  
   user = {
     username: 'Plamen',
-    age: 12
-  }
+    age: 12,
+    list: [1, 2, 3, 4, 5, 7, 8],
+  };
 
+  sum(a: number, b: number): number {
+    return a + b
+  }
 }
--- Pipes-Interceptors-and-Subjects/src/app/reduce.pipe.ts
+++ Pipes-Interceptors-and-Subjects/src/app/reduce.pipe.ts
@@ -1,12 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'reduce'
+  name: 'reduce',
+  //pure: false => not pure function will re-render
+  //pure: true => pure function will be memoized
 })
-export class ReducePipe implements PipeTransform {
-
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+export class ReducePipe<T> implements PipeTransform {
+  transform(
+    array: T[],
+    reduceFn: (acc: any, curr: T) => any,
+    initialValue: T
+  ): unknown {
+    return array.reduce(reduceFn, initialValue);
   }
-
 }
