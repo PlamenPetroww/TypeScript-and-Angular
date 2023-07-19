4b1f68c4d110a776d7e4083bbf23a699effbadb1
plamenpetroww <plamenpetroww@gmail.com>
2023-07-19 10:46:47 +0200

    make reducePipe

--- Pipes-Interceptors-and-Subjects/src/app/app.component.ts
+++ Pipes-Interceptors-and-Subjects/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Pipes-Interceptors-and-Subjects';
+  
+  user = {
+    username: 'Plamen',
+    age: 12
+  }
+
 }
