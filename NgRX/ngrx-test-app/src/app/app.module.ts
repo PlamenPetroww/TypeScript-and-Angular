3af9421044dbdbaf573799c8b624ffd7bce2d4fc
plamenpetroww <plamenpetroww@gmail.com>
2023-07-26 05:51:56 +0200

    console.log(eventCollection)

--- NgRX/ngrx-test-app/src/app/app.module.ts
+++ NgRX/ngrx-test-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReduxSimulationComponent } from './redux-simulation/redux-simulation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReduxSimulationComponent
   ],
   imports: [
     BrowserModule,
