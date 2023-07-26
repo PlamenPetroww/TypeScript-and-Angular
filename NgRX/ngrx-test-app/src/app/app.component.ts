45c303defb699ded569b9b51e569f020ea3f2a9b
plamenpetroww <plamenpetroww@gmail.com>
2023-07-26 04:52:36 +0200

    change the acc and curr with state and actions

--- NgRX/ngrx-test-app/src/app/app.component.ts
+++ NgRX/ngrx-test-app/src/app/app.component.ts
@@ -3,21 +3,44 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.demoTest();
   }
 
-  
-
   demoTest() {
-    function reducer(acc:number, curr: number) {
-      return acc + curr;
-    }
     //Redux => Design Pattern
-    const sum = [1, 2, 3].reduce(reducer, 0)
-    console.log(sum)
+
+    //type
+    interface AppState {
+
+    }
+
+    //Default state
+    const initialState = 0;
+
+    //Actions
+    const EVENT_ONE = 'EVENT_1'
+    const EVENT_TWO = 'EVENT_2'
+    const EVENT_THREE = 'EVENT_3'
+
+
+
+    //reducer
+    function reducer(state: AppState, actions: any) {
+      //return acc + curr;
+    }
+
+    //combine events with reducer and initial state
+    const eventCollection = [
+      { type: EVENT_ONE, value: 'test123' },
+      { type: EVENT_TWO, value: { name: 'Pesho', age: 21 } },
+      {type: EVENT_THREE, value: true}
+    ];
+
+    const result = eventCollection.reduce(reducer, initialState);
+    console.log(result);
   }
 }
