2ecf48c074dc6aeda17cef443a61b8bf53ee6ab8
plamenpetroww <plamenpetroww@gmail.com>
2023-08-01 12:57:26 +0200

    create isAuth function for login, register and logout

--- angular-project-exam/src/app/core/header/header.component.ts
+++ angular-project-exam/src/app/core/header/header.component.ts
@@ -1,20 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/user/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
 
-  isLogged = true;
+  email: any;
+  isAuth = false;
+  private userSub?: Subscription;
 
-  constructor() {
-
-  }
+  constructor(private authService: AuthService,
+    private router: Router) {}
 
   ngOnInit(): void {
+    this.userSub = this.authService.currentUser$.subscribe(user => {
+      this.isAuth = !user ? false : true;
+      this.email = user?.email;
+    })
+  }
 
+  ngOnDestroy(): void {
+    this.userSub?.unsubscribe();
+  };
+
+  onLogout() {
+    this.authService.logout();
+    this.router.navigate(['/']);
   }
 
 }
